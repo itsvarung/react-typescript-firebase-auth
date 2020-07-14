9689147156bc337013ef5f045e85e7fabe91d9e4
Varun <reachvarung@gmail.com>
2020-07-14 16:03:10 +0100

    - changed logged out view on account page so it gives user some information about whats gone wrong

--- frontend/src/pages/accountdetails/index.tsx
+++ frontend/src/pages/accountdetails/index.tsx
@@ -1,11 +1,9 @@
 import React, { useState, useEffect } from "react";
 import NavBar from "../../components/nav-bar";
-import Header from "../../components/accountdetails/header";
 import HeaderLoading from "../../components/accountdetails/headerloading";
 import HeaderLoggedOut from "../../components/accountdetails/headerloggedout";
 import Section from "../../components/accountdetails/section";
 import * as Styles from "./styles";
-import { Form, InputType, TypeOfData } from "../../models/Form";
 import { getCurrentFirstname } from "../../services/firebase";
 import "firebase/auth";
 import "firebase/firestore";
@@ -33,22 +31,13 @@ const AccountDetails: React.FC<Props> = (props) => {
     history.push("/signup");
   };
 
-  return isLoading ? (
-    <React.Fragment>
-      <NavBar firstname={"Obi Wan"} />
-      <Styles.MainWrapper>
-        <HeaderLoading />
-      </Styles.MainWrapper>
-    </React.Fragment>
-  ) : name ? (
+  return name ? (
+    // If user is logged in show them:
     <React.Fragment>
       <NavBar firstname={getCurrentFirstname() || "stranger"} />
-      <Styles.MainWrapper>
-        <Header />
-        <Section title="" description="" forms={formDetails} />
-      </Styles.MainWrapper>
     </React.Fragment>
   ) : (
+    // If user is not logged in show them a log in screen
     <React.Fragment>
       <NavBar firstname={"stranger"} />
       <Styles.MainWrapper>
@@ -59,53 +48,3 @@ const AccountDetails: React.FC<Props> = (props) => {
 };
 
 export default AccountDetails;
-
-const formDetails: Form[] = [
-  {
-    id: 0,
-    title: "Basic Details",
-    description: "Alexander or Alexandra? Check your basic details here.",
-    cardColor: "#1E2937",
-    url: "form",
-    progress: 10,
-    formSections: [
-      {
-        typeOfData: TypeOfData.basicDetails,
-        title: "Basic Details",
-        subtitle: "Tell us a little bit about you",
-        fields: [
-          {
-            label: "First Name",
-            helperText: "John",
-            inputType: InputType.firstname,
-          },
-          {
-            label: "Last Name",
-            helperText: "Doe",
-            inputType: InputType.lastname,
-          },
-        ],
-      },
-    ],
-  },
-  {
-    id: 1,
-    title: "Address Details",
-    description:
-      "Couldn't wait to leave six months ago? Check your address here.",
-    cardColor: "#1E2937",
-    url: "form",
-    progress: 20,
-    formSections: [],
-  },
-  {
-    id: 2,
-    title: "NHS Details",
-    description:
-      "Thought you were an A+ student, but turns out you're more B-? Check your NHS inputs here.",
-    cardColor: "#1E2937",
-    url: "form",
-    progress: 70,
-    formSections: [],
-  },
-];
